3899e134f3513012ba9c913ebea381c83af29f8c
jinglong485 <guojinglong1992@gmail.com>
2019-09-18 20:26:00 -0500

    haha

--- echeveria.py
+++ echeveria.py
@@ -9,7 +9,7 @@ def check_time(n):
         return False
 
 def main():
-    sleep_minutes = 15
+    sleep_minutes = 55
     flag = False
     
     while True:
@@ -17,9 +17,9 @@ def main():
         time.sleep(sleep_minutes*60)
 
         for i in range(24):
-        	if check_time(i):
-        		flag = True
-    	#check_time(13)
+            if check_time(i):
+                flag = True
+#        flag = check_time(13)
         if flag == True:
             get_picture()
             get_picture()
@@ -30,18 +30,18 @@ def main():
         else:
             print('Not right time!')
 
-def get_picture(path = './',folder = 'records/'):
-	folder_path = "".join((path,folder))
-	if not os.path.exists(folder_path):
-		os.mkdir(folder_path)
-	time.sleep(0.5)
-	cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-	ret, frame = cap.read()
-	cap.release()
-	cv2.destroyAllWindows() 
-	file_name = str(time.strftime("%Y-%m-%d_%H-%M-%S",time.localtime())) + '.jpg'
-	whole_file_path = ''.join((folder_path,file_name))
-	print('saving file to: {}'.format(whole_file_path))
-	cv2.imwrite(whole_file_path, frame, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
+def get_picture(path = './',folder = 'echeveria-records/'):
+    folder_path = "".join((path,folder))
+    if not os.path.exists(folder_path):
+        os.mkdir(folder_path)
+    time.sleep(0.5)
+    cap = cv2.VideoCapture(0)#, cv2.CAP_DSHOW)
+    ret, frame = cap.read()
+    cap.release()
+    cv2.destroyAllWindows() 
+    file_name = str(time.strftime("%Y-%m-%d_%H-%M-%S",time.localtime())) + '.jpg'
+    whole_file_path = ''.join((folder_path,file_name))
+    print('saving file to: {}'.format(whole_file_path))
+    cv2.imwrite(whole_file_path, frame, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
 
 main()
---
+++ tmux.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd ~/Echeveria-project/
+tmux new-session -d -s echeveria 'python3 ~/Echeveria-project/echeveria.py'
